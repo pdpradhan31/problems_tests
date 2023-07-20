c3312a20e1877a750a48397e41b7d7a84db60eab
Padmini <pdpradhan31@gmail.com>
2023-07-20 12:55:12 +0100

    Grouping Array Element Problem

---
+++ groupingElementProblem.js
@@ -0,0 +1,25 @@
+function groupArrayElements(array, cols) {
+    function split(array, cols) {
+        if (cols==1) return array;
+        var size = Math.ceil(array.length / cols);
+        return array.slice(0, size).concat([null]).concat(split(array.slice(size), cols-1));
+    }    
+    var a = split(array, cols);
+    var groups = [];
+    var group = [];
+    for(var i = 0; i < a.length; i++) {
+        if (a[i] === null) {
+            groups.push(group);
+            group = [];
+            continue;
+         }
+        group.push(a[i]);  
+    }
+    groups.push(group);
+    return groups;
+}
+
+
+var arr = [1, 2, 3, 4, 5];
+let noOfDivision = 3
+console.log(groupArrayElements(arr, noOfDivision));
